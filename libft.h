5a132a98faa7de736f312e69c41905821f21e06a
El Mostafa El Bahtouri <elel-bah@e1r6p11.1337.ma>
2023-11-06 18:39:41 +0100

    adding ft_strcpy

--- libft.h
+++ libft.h
@@ -3,6 +3,7 @@
 
 #include <stdio.h>
 #include <string.h>
+char	*ft_strcpy(char *dst, const char *src);
 int	ft_memcmp(void *s1, void *s2, unsigned int n);
 int	ft_isalnum(int x);
 int	ft_atoi(const char *str);
