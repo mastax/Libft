873c2aaede9316aea272ca45f4fa6f5798da3592
El Mostafa El Bahtouri <elel-bah@e1r4p5.1337.ma>
2023-11-10 19:33:44 +0100

    editing libft header file

--- libft.h
+++ libft.h
@@ -1,13 +1,26 @@
-#ifndef _LIBFT_H
-#define _LIBFT_H
+#ifndef LIBFT_H
+# define LIBFT_H
+#include <stddef.h>
+#include <stdlib.h>
+int		ft_isprint(int x);
+int		ft_isdigit(int x);
+int		ft_isascii(int x);
+int		ft_isalpha(int x);
+int		ft_isalnum(unsigned char x);
+int		ft_atoi(char *str);
+int		ft_strncmp(const char *s1, const char *s2, size_t n);
+void	*ft_calloc(size_t count, size_t size);
+int		ft_memcmp(const void *s1, const void *s2, unsigned int n);
+size_t	ft_strlcpy(char *dst, const char *src, size_t ds_size);
+size_t	ft_strlcat(char *dst, const char *src, size_t dst_size);
+void	*ft_memmove(void *dst, const void *src, unsigned int n);
+char	*ft_strncat(char *dst, const char *src, size_t nb);
+void	*ft_memcpy(void *dest, const void *src, unsigned int n);
+void	ft_bzero(void *s, unsigned int n);
+void	*ft_memset(void *s, int c, unsigned int len);
+char	*ft_strdup(char *src);
+int		tolower(int x);
+int		ft_toupper(int x);
+int		ft_strlen(const char *str);
 
-#include <stdio.h>
-#include <string.h>
-char	*ft_strcpy(char *dst, const char *src);
-int	ft_memcmp(void *s1, void *s2, unsigned int n);
-int	ft_isalnum(int x);
-int	ft_atoi(const char *str);
-void    *ft_memcpy(void *dest, const void *src, unsigned int n);
-void    ft_bzero(void *s, unsigned int n);
-void    *ft_memset(void *s, int c, unsigned int n);
 #endif
